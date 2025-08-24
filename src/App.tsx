faf989d6ac8f8c5e39555467b534098d0613bb98
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-08-24 13:39:41 +0600

    implement react query setup for the project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div className="bg-red-500 text-white h-screen w-screen flex items-center justify-center text-4xl font-bold">App</div>
+  )
+}
+
+export default App
